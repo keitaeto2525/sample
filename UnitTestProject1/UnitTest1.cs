61715efadea7a2e4d89bb4ca4f8aec72d80e6376
Masaru Takahashi <matakaha@microsoft.com>
2019-09-03 13:43:06 +0900

    first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using FunctionApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Function1.SampleMethod("AAA"));
+        }
+    }
+}
